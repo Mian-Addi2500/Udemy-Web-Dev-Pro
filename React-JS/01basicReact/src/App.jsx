b84fe1492e0c29c68ebc3f26251c8220e95fc938
Mian-Addi2500 <mian.kami2500@gmail.com>
2025-06-30 15:01:56 +0500

    01basic react project

---
+++ React-JS/01basicReact/src/App.jsx
@@ -0,0 +1,15 @@
+import Chai from "./Chai";
+import "./App.css";
+
+function App() {
+  const userName = "Adil Mian";
+  return (
+    <>
+      <Chai />
+      <h1>My First React App</h1>
+      <h2>Welcome, {userName}!</h2>
+    </>
+  );
+}
+
+export default App;
---
+++ React-JS/01basicReact/src/Chai.jsx
@@ -0,0 +1,9 @@
+function Chai() {
+  return (
+    <>
+      <h2>New componenet</h2>
+    </>
+  );
+}
+
+export default Chai;
